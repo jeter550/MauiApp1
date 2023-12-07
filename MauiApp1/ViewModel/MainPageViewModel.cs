bf1a46c6154d2b3328cd0d654f33f0918a651976
Jéter Ornelas <jeteroliveira@hotmail.com>
2023-12-07 02:20:50 -0300

    Add project files.

---
+++ MauiApp1/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.ViewModel
+{
+    internal class MainPageViewModel : ViewModelBase
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private string _nome;
+        public string Nome
+        {
+            get
+            {
+                return this._nome;
+            }
+            set
+            {
+                this._nome = value;
+                this.OnPropertyChanged(() => Nome);
+               
+            }
+        }
+    }
+}
---
+++ MauiApp1/ViewModel/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.ViewModel
+{
+    abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged<T>(Expression<Func<T>> f)
+        {
+            string name = ((MemberExpression)f.Body).Member.Name;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
